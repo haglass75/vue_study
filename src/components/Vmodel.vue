001c9e1ed4dc07f49efce184385bd6bb588c0b36
ha hyungmi <haglass75@gmail.com>
2025-03-05 17:43:46 +0900

    v-model 드롭다운기능 추가

--- src/components/Vmodel.vue
+++ src/components/Vmodel.vue
@@ -3,6 +3,7 @@ import { ref } from "vue";
 const name = ref("");
 const text = ref("");
 const checked = ref(false);
+const selected = ref("apple")
 </script>
 <template>
   <div>
@@ -14,6 +15,13 @@ const checked = ref(false);
   <div>
     <label> <input type="checkbox" v-model="checked" /> 동의합니다. </label>
     <p>체크 상태: {{ checked }}</p>
+    <hr>
+    <select v-model="selected">
+    <option value="apple">사과</option>
+    <option value="banana">바나나</option>
+    <option value="grape">포도</option>
+  </select>
+  <p>선택한 과일: {{ selected }}</p>
   </div>
 </template>
 <style scoped></style>
