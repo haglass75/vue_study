824c1776b585a01583ddf8a8814195c3eb6784d1
haglass75 <133125113+haglass75@users.noreply.github.com>
2025-03-10 15:30:30 +0900

    감소 버튼 추가

--- src/components/EventHandl.vue
+++ src/components/EventHandl.vue
@@ -5,6 +5,7 @@ const count = ref(0)
 </script>
 <template>
 <button @click="count++">클릭수 : {{ count }}</button>
+<button @click="count--">클릭수 : {{ count }}</button>
 </template>
 <style scoped>
 </style>
